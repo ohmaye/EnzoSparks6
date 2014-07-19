48bb5ad1174239785c735451e6e85d22a0dbc970
Enio Ohmaye <enioohmaye@Enios-MacBook-2.local>
2014-07-19 16:34:30 +0800

    q

--- EnzoSparks6/GameScene.swift
+++ EnzoSparks6/GameScene.swift
@@ -9,11 +9,14 @@
 import SpriteKit
 
 extension SKEmitterNode {
-    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
-        let action = SKAction.scaleBy(2.0, duration: 0.5)
+    func grow() {
+        self.removeAllActions()
+        let action1 = SKAction.scaleBy(2.0, duration: 0.5)
         let action2 = SKAction.scaleBy(0.5, duration: 0.5)
-        let action3 = SKAction.sequence([action, action2])
-        self.runAction(action3)
+        let action3 = SKAction.waitForDuration(2.0)
+        let action4 = SKAction.removeFromParent()
+        let actions = SKAction.sequence([action1, action2, action3, action4])
+        self.runAction(actions)
     }
 }
 
@@ -27,65 +30,42 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
-        let location = touches.anyObject().locationInNode(self)
-        if !emitter {
-            if let emit = loadNode("SparkParticle") as? SKEmitterNode {
-                emitter = emit
-                emit.position = location
-                self.addChild(emit)
+        
+        for touch: AnyObject in touches! {
+            let location = touch.locationInNode(self)
+            if let node = nodeAtPoint(location) as? SKEmitterNode {
+                // If an emitter was touched, then grow it.
+                node.grow()
+            } else {
+                // If not, then create one.
+                addEmitter(location)
             }
         }
-        if let emit = emitter? {
-            println(emit.containsPoint(location))
-            println(emit.calculateAccumulatedFrame())
-            println(emit.frame)
-            //
-            emit.position = location
-
-        }
-        
     }
     
-    /*
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-    */
-   
+    
+
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
     
     /* Helper functions */
     
-    func showEmitter( name : String, position : CGPoint) -> SKEmitterNode {
-        let path = NSBundle.mainBundle().pathForResource(name, ofType: "sks")
-        let emitter = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as? SKEmitterNode
-        emitter!.position = position
-        self.addChild(emitter!)
-        return emitter!
-    }
-    
     func loadNode<NodeClass> ( name : String) -> NodeClass?  {
         let path = NSBundle.mainBundle().pathForResource(name, ofType: "sks")
         let node = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as? NodeClass
         return node
+    }
+    
+    func addEmitter(location: CGPoint) {
+        if let emit = loadNode("SparkParticle") as? SKEmitterNode {
+            emit.position = location
+            let action1 = SKAction.waitForDuration(2.0)
+            let action2 = SKAction.removeFromParent()
+            let actions = SKAction.sequence([action1, action2])
+            emit.runAction(actions)
+            self.addChild(emit)
+        }
     }
 
 }
